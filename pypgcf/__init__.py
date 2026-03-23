7c1c9690a605b71609bb7347a65fc50166480add
Marios Nikolaidis <marionik23@gmail.com>
2026-03-23 08:20:30 -0500

    Added test data on dev

--- pypgcf/__init__.py
+++ pypgcf/__init__.py
@@ -3,6 +3,6 @@ pyPGCF: PhyloGenomic, Core and Fingerprint analyses package
 author: Marios Nikolaidis
 """
 
-from . import _version
+# from . import _version
 
-__version__ = _version.get_versions()["version"]
+# __version__ = _version.get_versions()["version"]
